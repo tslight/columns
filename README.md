8802dfa78eb58817a952896f469d45dde437c5a9
tslight <tobyslight@gmail.com>
2018-07-26 01:48:20 +0100

    add some pictures

--- README.md
+++ README.md
@@ -2,6 +2,24 @@
 
 *Does what it says on the tin.*
 
+![img](./fullscreen.png "Fullscreen Terminal")
+
+![img](./80x24.png "80 x 24 Terminal")
+
 ## INSTALLATION
 
 `pip install columns`
+
+## CLI USAGE
+
+```
+usage: columns [-h] list [list ...]
+
+Print a list in columns.
+
+positional arguments:
+  list        A list to print.
+
+optional arguments:
+  -h, --help  show this help message and exit
+```
--- columns/__main__.py
+++ columns/__main__.py
@@ -1,15 +1,15 @@
 def getargs():
     from argparse import ArgumentParser
     parser = ArgumentParser(description='Print a list in columns.')
-    parser.add_argument("lst", nargs='+', help="A list to print.")
+    parser.add_argument("list", nargs='+', help="A list to print.")
     return parser.parse_args()
 
 
 def main():
     from .columns import prtcols
     args = getargs()
-    lst = args.lst
-    prtcols(lst)
+    l = args.list
+    prtcols(l)
 
 
 if __name__ == '__main__':
